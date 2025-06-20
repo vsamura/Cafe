efe126f2f8a65f28d7ef43efb219aaa09da1a986
Vladislav Samura <vx5080@yandex.ru>
2025-06-20 17:49:43 +0900

    Lesson 4 end

--- app/src/main/java/com/example/cafe/MaceOrderActivity.java
+++ app/src/main/java/com/example/cafe/MaceOrderActivity.java
@@ -1,6 +1,9 @@
 package com.example.cafe;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,6 +13,15 @@ public class MaceOrderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mace_order);
+        TextView greetings = findViewById(R.id.textViewGreetings);
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("name");
+        greetings.setText(getResources().getString(R.string.hello_what, name));
+    }
 
+    public static Intent newIntent(Context context, String userName){
+        Intent intent = new Intent(context, MaceOrderActivity.class);
+        intent.putExtra("name", userName);
+        return intent;
     }
 }
--- app/src/main/java/com/example/cafe/MainActivity.java
+++ app/src/main/java/com/example/cafe/MainActivity.java
@@ -2,20 +2,50 @@ package com.example.cafe;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 public class MainActivity extends AppCompatActivity {
+    protected EditText name;
+    protected EditText password;
+    protected Button enter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(this, OrderDetallActivity.class);
+        initviews();
+
+        enter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userName = name.getText().toString().trim();
+                String userPassword = password.getText().toString().trim();
+                if(!userName.isEmpty() && !userPassword.isEmpty()){
+                    launchNextScreen(userName);
+                } else {
+                    Toast.makeText(
+                            MainActivity.this,
+                            getString(R.string.error),
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void launchNextScreen(String userName){
+        Intent intent = MaceOrderActivity.newIntent(this, userName);
         startActivity(intent);
+    }
+
+    private void initviews(){
+        name = findViewById(R.id.editTextName);
+        password = findViewById(R.id.editTextPassword);
+        enter = findViewById(R.id.buttonEnter);
     }
 }
