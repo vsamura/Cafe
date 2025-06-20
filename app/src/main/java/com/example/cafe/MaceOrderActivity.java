018e56d5865cc685318c292321b32a2984791c96
Vladislav Samura <vx5080@yandex.ru>
2025-06-20 20:00:55 +0900

    Lesson 5 start

--- app/src/main/java/com/example/cafe/MaceOrderActivity.java
+++ app/src/main/java/com/example/cafe/MaceOrderActivity.java
@@ -3,25 +3,60 @@ package com.example.cafe;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MaceOrderActivity extends AppCompatActivity {
+
+    private static final String USER_NAME = "name";
+
+    private TextView textViewgGreetings;
+    private RadioGroup radioGroup;
+    private RadioButton radioButtonTee;
+    private RadioButton radioButtonCoffe;
+    private TextView textViewAdditives;
+    private CheckBox checkBoxMilk;
+    private CheckBox checkBoxSahar;
+    private CheckBox checkBoxLemon;
+    private Spinner spinnerTee;
+    private Spinner spinnerCoffe;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mace_order);
-        TextView greetings = findViewById(R.id.textViewGreetings);
-        Intent intent = getIntent();
-        String name = intent.getStringExtra("name");
-        greetings.setText(getResources().getString(R.string.hello_what, name));
+        initviews();
+        setupUserName();
+
+    }
+
+    private void setupUserName(){
+        String name = getIntent().getStringExtra(USER_NAME);
+        String resultName = getString(R.string.hello_what, name);
+        textViewgGreetings.setText(resultName);
+    }
+
+    private void initviews(){
+        textViewgGreetings = findViewById(R.id.textViewGreetings);
+        radioGroup = findViewById(R.id.radioGroup);
+        radioButtonTee = findViewById(R.id.radioTee);
+        radioButtonCoffe = findViewById(R.id.radioCoffe);
+        textViewAdditives = findViewById(R.id.textViewAdditives);
+        checkBoxMilk = findViewById(R.id.checkBoxMilk);
+        checkBoxSahar = findViewById(R.id.checkBoxSahar);
+        checkBoxLemon = findViewById(R.id.checkBoxLemon);
+        spinnerTee = findViewById(R.id.spinerTee);
+        spinnerCoffe = findViewById(R.id.spinerCoffe);
     }
 
     public static Intent newIntent(Context context, String userName){
         Intent intent = new Intent(context, MaceOrderActivity.class);
-        intent.putExtra("name", userName);
+        intent.putExtra(USER_NAME, userName);
         return intent;
     }
 }
